55a47004559f1d689054bfc684fb9440e85e079a
fontanierh <henry.fontanier@alan.eu>
2021-12-30 12:34:26 +0100

    remove irrelevant comment

--- contracts/CryptoMarriage.sol
+++ contracts/CryptoMarriage.sol
@@ -17,7 +17,6 @@ contract CryptoMarriage is ERC721, ERC721Burnable, AccessControl {
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
   Marriage[] public marriages;
 
-  /// @custom:oz-upgrades-unsafe-allow constructor
   constructor() ERC721("CryptoMarriage", "CM") {
     _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _setupRole(MINTER_ROLE, msg.sender);
